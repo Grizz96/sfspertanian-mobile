d072ef200087fe6c09c541c484851c78e5bd33b3
caesarali18 <143463709+caesarali18@users.noreply.github.com>
2023-11-05 20:04:00 +0700

    add activity_rating.xml

---
+++ app/src/main/java/com/example/sfspertanian/RatingActivity.java
@@ -0,0 +1,28 @@
+package com.example.sfspertanian;
+
+import android.os.Bundle;
+import android.transition.Fade;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sfspertanian.R;
+
+public class RatingActivity extends AppCompatActivity {
+    ImageButton btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Menerapkan transisi masuk
+        getWindow().setEnterTransition(new Fade());
+        getWindow().setExitTransition(new Fade());
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v->{
+            finish();
+        });
+
+    }
+}
