1c49c51888efac699c19b280f1d81704e477724b
fuadadhim24 <fuadadhim24@gmail.com>
2023-12-01 21:24:21 +0700

    Update db_contract

--- app/src/main/java/com/example/sfspertanian/Db_Contract.java
+++ app/src/main/java/com/example/sfspertanian/Db_Contract.java
@@ -3,8 +3,8 @@ package com.example.sfspertanian;
 public class Db_Contract {
 
 
-        public static final String urlRegister = "https://paditracer.000webhostapp.com/api_register.php";
-        public static final String urlLogin = "https://paditracer.000webhostapp.com/api_login.php";
+        public static final String urlRegister = "https://jejakpadi-develop.000webhostapp.com/mobileController/api_register.php";
+        public static final String urlLogin = "https://jejakpadi-develop.000webhostapp.com/mobileController/api_login.php";
 
         public static final String urlSavemap = "https://192.168.0.106/api_login.php";
 }
