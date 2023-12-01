844d9ca4327ec2e339eac2c0a81968e049a88858
fuadadhim24 <fuadadhim24@gmail.com>
2023-12-02 00:48:29 +0700

    Update view location

--- app/src/main/java/com/example/sfspertanian/MapsFragment.java
+++ app/src/main/java/com/example/sfspertanian/MapsFragment.java
@@ -1,18 +1,36 @@
 package com.example.sfspertanian;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MapsFragment extends AppCompatActivity {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private WebView webView;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +60,80 @@ public class MapsFragment extends AppCompatActivity {
         // Add JavaScript interface for communication between WebView and Android
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
+        // Initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         // Load the Leaflet map HTML
         webView.loadUrl("file:///android_asset/leaflet_map.html");
+
+        // Set up action when the FAB is clicked
+        FloatingActionButton fabGetLocation = findViewById(R.id.btnGetLocation);
+        fabGetLocation.setOnClickListener(v -> {
+            // Check location permission
+            checkLocationPermission();
+        });
+
+        // Initialize the location callback
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    // Call the JavaScript function from WebView
+                    webView.loadUrl("javascript:getCurrentLocation(" + latitude + "," + longitude + ")");
+                }
+            }
+        };
+    }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // If permission is already granted, request location
+            requestLocationUpdates();
+        } else {
+            // If permission is not granted, request it from the user
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void requestLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is granted, request location updates
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        } else {
+            // Permission is not granted, request it from the user
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, request location updates
+                requestLocationUpdates();
+            } else {
+                // Permission denied, inform the user or handle accordingly
+                Log.e("Location Permission", "Permission denied");
+            }
+        }
     }
 
     // Interface to handle JavaScript calls from WebView
