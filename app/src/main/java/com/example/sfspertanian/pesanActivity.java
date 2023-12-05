47c2b623566208d3729b8e911574451b55c90ed4
rzkprdk27 <rizkypradika27@gmail.com>
2023-12-05 22:33:55 +0700

    Update Chat Admin

--- app/src/main/java/com/example/sfspertanian/pesanActivity.java
+++ app/src/main/java/com/example/sfspertanian/pesanActivity.java
@@ -9,6 +9,7 @@ import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.view.View;
 import android.widget.Button;
+import android.net.Uri;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class pesanActivity extends AppCompatActivity {
 
     ImageButton btnBack;
+    Button btnwaadmin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -26,9 +29,23 @@ public class pesanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pesan);
 
         btnBack = findViewById(R.id.backToHalamanBefore);
-        btnBack.setOnClickListener(v->{
-            finish();
-        });
+        btnwaadmin = findViewById(R.id.btnwaadmin);
+        btnBack.setOnClickListener(v-> finish());
+        // Set an OnClickListener for the Chat Admin button
+        btnwaadmin.setOnClickListener(v -> {
+            // Replace "6287840199095" with the actual phone number
+            String phoneNumber = "6287840199095";
 
+            // Create a Uri with the WhatsApp number
+            Uri uri = Uri.parse("https://wa.me/" + phoneNumber);
+
+            // Create an Intent to open the WhatsApp app
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+            // Start the Intent
+            startActivity(intent);
+        });
     }
 }
+
+
